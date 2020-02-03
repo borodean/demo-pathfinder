195a7fb69312562fc523e0392042a9ac535701aa
Vadym Borodin <borodean@gmail.com>
2020-02-04 01:26:18 +0200

    Move the provider inclusion up to index.tsx

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
 
 import Grid from 'components/Grid';
-import configureStore from 'store';
-
-const store = configureStore();
 
 const App = () => {
-  return (
-    <ReduxProvider store={store}>
-      <Grid />
-    </ReduxProvider>
-  );
+  return <Grid />;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import App from 'components/App';
+import configureStore from 'store';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+const Root = () => (
+  <ReduxProvider store={store}>
+    <App />
+  </ReduxProvider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
