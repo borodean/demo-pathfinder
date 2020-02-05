ace57cbe16032d9532a1d832f688ab67ba24bd83
Vadym Borodin <borodean@gmail.com>
2020-02-05 15:29:56 +0200

    Updates

---
+++ precache-manifest.2ef2efff3ac952036c967d718b742b8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d527fc6267eeda94ca7e24c7d744093",
+    "url": "/demo-pathfinder/index.html"
+  },
+  {
+    "revision": "09d11c833053d87c70eb",
+    "url": "/demo-pathfinder/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ef2ba7bca0c45e43f5f4",
+    "url": "/demo-pathfinder/static/css/main.263b3cfb.chunk.css"
+  },
+  {
+    "revision": "09d11c833053d87c70eb",
+    "url": "/demo-pathfinder/static/js/2.c2fd5bee.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/demo-pathfinder/static/js/2.c2fd5bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2ba7bca0c45e43f5f4",
+    "url": "/demo-pathfinder/static/js/main.02c33315.chunk.js"
+  },
+  {
+    "revision": "a0b104765cce984c335f",
+    "url": "/demo-pathfinder/static/js/runtime-main.94b53b79.js"
+  }
+]);
